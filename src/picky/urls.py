e3cc7fb5e926c4067d2a69173e54ab58047c35cd
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-18 23:44:11 +0530

    Generic Display Views:DetailView

--- src/picky/urls.py
+++ src/picky/urls.py
@@ -19,13 +19,15 @@ from django.views.generic import TemplateView
 
 from restaurants.views import (
     RestaurantListView,
+    RestaurantDetailView,
 )
 
 
 
 urlpatterns = [
     path('restaurants/',RestaurantListView.as_view()),
-    path('restaurants/<slug>/',RestaurantListView.as_view()),
+    # path('restaurants/<slug>/',RestaurantListView.as_view()),
+    path('restaurants/<rest_id>/',RestaurantDetailView.as_view()),
     path('about/',TemplateView.as_view(template_name = "about.html")),
     path('contact/',TemplateView.as_view(template_name = "contact.html")),
     path('admin/', admin.site.urls),
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,18 +1,11 @@
 import random
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,DetailView
 
 from restaurants.models import RestaurantLocation
 # Create your views here.
-def restaurant_listview(request):
-    template_name = 'restaurants/restaurants_list.html'
-    queryset      = RestaurantLocation.objects.all()  #It will create a object list
-    context = {
-        "object_list":queryset,
-    }
-    return render(request,template_name,context)
 
 class RestaurantListView(ListView):
     def get_queryset(self):
@@ -25,3 +18,10 @@ class RestaurantListView(ListView):
         else:
             queryset = RestaurantLocation.objects.all()
         return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+    def get_object(self,*args,**kwargs):
+        rest_id = self.kwargs.get("rest_id")
+        obj = get_object_or_404(RestaurantLocation, pk=rest_id)
+        return obj
