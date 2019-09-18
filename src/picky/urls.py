8053742ed36e2c7434daa14e8532b953f2e846a9
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-18 23:12:46 +0530

    Generic ListView

--- src/picky/urls.py
+++ src/picky/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    RestaurantListView,
+)
+
 
 
 urlpatterns = [
-    path('restaurants/',restaurant_listview),
+    path('restaurants/',RestaurantListView.as_view()),
+    path('restaurants/<slug>/',RestaurantListView.as_view()),
     path('about/',TemplateView.as_view(template_name = "about.html")),
     path('contact/',TemplateView.as_view(template_name = "contact.html")),
     path('admin/', admin.site.urls),
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,14 +1,27 @@
 import random
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 
 from restaurants.models import RestaurantLocation
 # Create your views here.
 def restaurant_listview(request):
     template_name = 'restaurants/restaurants_list.html'
-    queryset      = RestaurantLocation.objects.all()  #It will create a object list 
+    queryset      = RestaurantLocation.objects.all()  #It will create a object list
     context = {
         "object_list":queryset,
     }
     return render(request,template_name,context)
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__icontains = slug) |
+                Q(category__iexact = slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
