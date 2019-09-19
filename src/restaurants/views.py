7816ae4cb28be2d3744f12f82f01e2672d0535fe
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-19 14:07:26 +0530

    The old school way of saving the forms using Html forms.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,11 +1,27 @@
 import random
 from django.db.models import Q
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.views.generic import TemplateView,ListView,DetailView
 
+from restaurants.forms import RestaurantCreateForm
 from restaurants.models import RestaurantLocation
 # Create your views here.
+
+def restaurant_createview(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        location = request.POST.get("location")
+        category = request.POST.get("category")
+        obj = RestaurantLocation.objects.create(
+                name = name,
+                location = location,
+                category = category
+            )
+        return HttpResponseRedirect("/restaurants/")
+    template_name = 'restaurants/form.html'
+    context = {}
+    return render(request,template_name,context)
 
 class RestaurantListView(ListView):
     def get_queryset(self):
@@ -21,4 +37,3 @@ class RestaurantListView(ListView):
 
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
-    
