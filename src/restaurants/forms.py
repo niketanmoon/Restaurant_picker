44617fef6431e2b0821604bdf839664c1692c2b0
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-19 16:17:16 +0530

    Applied validations on fields using validators.py file and done some capitalization in pre_save signals

--- src/restaurants/forms.py
+++ src/restaurants/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
 from restaurants.models import RestaurantLocation
+#from restaurants.validators import validate_category
 
 
 class RestaurantLocationCreateForm(forms.ModelForm):
+    #category = forms.CharField(required=False, validators=[validate_category])
     class Meta:
         model = RestaurantLocation
         fields = [
