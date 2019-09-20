d5172426f94bfc764bc6c95211b0110500839a01
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-21 01:22:48 +0530

    Added Profile model with followers and following functionality

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 
+User = settings.AUTH_USER_MODEL
 # Create your models here.
+class Profile(models.Model):
+    user        = models.OneToOneField(User,on_delete=models.CASCADE)
+    followers   = models.ManyToManyField(User, related_name = 'is_following', blank=True)
+    activated   = models.BooleanField(default=False)
+    timestamp   = models.DateTimeField(auto_now_add = True)
+    updated     = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.user.username
+
+def post_save_user_receiver(sender, instance,created,*args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user = instance)
+
+post_save.connect(post_save_user_receiver,sender=User)
