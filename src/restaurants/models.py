4acd00478c9bb7856bbf9c973ab93e62245e0682
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-21 00:29:00 +0530

    Added User Search Field with Manager and Q lookups

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,12 +1,29 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.db.models import Q
 from django.db.models.signals import pre_save
 from .utils import unique_slug_generator
 
 from restaurants.validators import validate_category
 
 User = settings.AUTH_USER_MODEL
+
+class RestaurantLocationQuerySet(models.query.QuerySet):
+    def search(self,query):
+        return self.filter(
+            Q(name__icontains = query) |
+            Q(location__icontains=query) |
+            Q(category__icontains=query)|
+            Q(item__name__icontains=query)|
+            Q(item__contents__icontains=query)
+        ).distinct()
+
+class RestaurantLocationManager(models.Manager):
+    def get_queryset(self):
+        return RestaurantLocationQuerySet(self.model,using=self._db)
+    def search(self,query):
+        return self.get_queryset().search(query)
 # Create your models here.
 class RestaurantLocation(models.Model):
     owner       = models.ForeignKey(
@@ -19,6 +36,8 @@ class RestaurantLocation(models.Model):
     timestamp   = models.DateTimeField(auto_now_add=True, null=True)
     updated     = models.DateTimeField(auto_now=True)
     slug        = models.SlugField(null=True, blank=True)
+
+    objects = RestaurantLocationManager() #Model.objects.all()
 
     def __str__(self):
         return self.name
